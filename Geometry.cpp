c9e09f2f443b756a1ca01afa8ec8f69226df535e
Yoon-SeokJin <63408412+Yoon-SeokJin@users.noreply.github.com>
2021-07-14 15:09:52 +0900

    Update and rename Convex_hull.cpp to Geometry.cpp

--- Geometry.cpp
+++ Geometry.cpp
@@ -54,10 +54,11 @@ ll ccw(const v2& a, const v2& b, const v2& c) {
 
 vector<v2> get_hull(vector<v2> pt) {
     /*
-    주어진 점들 중 볼록껍질에 속하는 점들을 반환.
+    주어진 점들 중 볼록껍질에 속하는 점들을 반환
+    모노톤 체인 알고리즘을 사용 (O(logN), N=pt.size())
     
     Property:
-        -pt(vector<v2>): points, 볼록껍질을 구할 여러 점들의 정보가 담긴 벡터
+        -pt(vector<v2>): points, 볼록껍질을 구할 여러 점들의 정보가 담긴 벡터, 순서는 상관없다.
     
     Return:
         -vector<v2>: 볼록껍질에 속하는 점 중 y가 가장 작은 점에서 시작하여 반시계방향순으로 저장된 배열
@@ -76,4 +77,29 @@ vector<v2> get_hull(vector<v2> pt) {
     }
     r.insert(end(r), rbegin(l) + 1, rend(l) - 1);
     return r;
+}
+
+pair<v2, v2> get_farthest_pair(const vector<v2>& pt) {
+    /*
+    볼록껍질이 주어졌을 때 가장 먼 두 점을 반환
+    회전하는 캘리퍼스 알고리즘을 사용 (O(N), N=pt.size())
+    
+    Property:
+        -pt(vector<v2>): points, 볼록껍질에 속하는 점의 정보가 담긴 벡터, 순서는 반드시 반시계방향순이어야 한다.
+    */
+    double dist = 0;
+    v2 ans1, ans2;
+    for (int i = 0, a = 0, b = 1; i < ssize(pt) * 2; ++i) {
+        int na = (a + 1) % ssize(pt);
+        int nb = (b + 1) % ssize(pt);
+        if (double ndist = (pt[a] - pt[b]).norm(); ndist > dist) {
+            dist = ndist;
+            ans1 = pt[a];
+            ans2 = pt[b];
+        }
+        int cross = ccw(pt[na] - pt[a], pt[nb] - pt[b]);
+        if (cross > 0) b = nb;
+        else a = na;
+    }
+    return {ans1, ans2};
 }
